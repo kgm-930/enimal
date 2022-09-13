8abfa9b75023c5c5a0a8242435e400c9c5be7e6f
haengsong <haengsong@naver.com>
2022-09-13 14:25:52 +0900

    setup : 프론트엔드 초기세팅

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "@styles/reset.css";
+import "@styles/_typography.scss";
+import "@styles/_common.scss";
+import "@styles/font.css";
+// import Router from "./routers/Router";
+
+
+function App() {
+  // return <Router />;
+  <div>시작</div>
+  return (
+    <div>테스트테스트</div>
+  )
+}
+
+export default App;
