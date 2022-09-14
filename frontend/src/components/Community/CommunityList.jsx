2a25dc2436025382d047b352e84a12eab61dcdff
이동명 <aklsjd7380@naver.com>
2022-09-15 06:41:04 +0900

    feat : 커뮤니티메인페이지 퍼블리싱

---
+++ frontend/src/components/Community/CommunityList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./CommunityCard.scss";
+import CommunityCard from "./CommunityCard";
+
+
+function CommunityList() {
+  return (
+    <div className="cardList flex">
+      <CommunityCard />
+      <CommunityCard />
+      <CommunityCard />
+      <CommunityCard />
+      <CommunityCard />
+      <CommunityCard />
+      <CommunityCard />
+      <CommunityCard />
+      <CommunityCard />
+    </div>
+
+    
+  )
+}
+
+export default CommunityList;
