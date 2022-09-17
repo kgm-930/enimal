19d0983694eb198a0c4ef4632f7ec46e8bcc4c1f
이동명 <aklsjd7380@naver.com>
2022-09-17 13:24:43 +0900

    feat : 글등록페이지 퍼블리싱 # 15

--- frontend/src/screens/Community/CommunityRegist.jsx
+++ frontend/src/screens/Community/CommunityRegist.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./CommunityRegist.scss";
 
+import clickLogo from "@assets/icons/ads_click_black_24dp.svg"
+
 function CommunityRegist() {
-  return <div>하기싫당</div>;
+  return (
+    <div className="container flex">
+      <div className="regi">
+        <div className="regi_pagetitle notoBold fs-36">
+          자랑게시판
+        </div>
+        <div className="divide" />
+        <div className="regi_content flex justify-center">
+          <div className="regi_content_img">
+            <img src={clickLogo} alt="클릭로고" />
+          </div>
+        </div>
+        <div className="regi_text">
+          <div className="regi_text_title notoMid fs-24">
+            제목
+          </div>
+          <div className="divide" />
+          <div className="regi_text_write notoReg fs-20">
+            내용을 입력해주세요.
+          </div>
+        </div>
+        <div className="regi_button flex">
+          <button type="button" className="notoBold fs-24">
+            등록하기
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CommunityRegist;
