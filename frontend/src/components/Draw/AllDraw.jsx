30cb9370c6b171e3463866e042eecada2b4739f3
haengsong <haengsong@naver.com>
2022-09-20 22:18:17 +0900

    feat : 선택뽑기 퍼블리싱

--- frontend/src/components/Draw/AllDraw.jsx
+++ frontend/src/components/Draw/AllDraw.jsx
@@ -105,10 +105,10 @@ function AllDraw() {
           <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
         </div>
       </div>
-      <button className="drawButton fs-28 notoBold" id="collection" type="button">전체뽑기 (가격)</button>
+      <button className="drawButton fs-28 notoBold" id="collection" type="button">전체 뽑기 (가격)</button>
       <div>
         <h1 className="fs-24 notoBold drawNotice">뽑기를 통해 컬렉션을 수집해보세요.</h1>
-        <h1 className="fs-24 notoBold drawNotice">전체 뽑기는 216종류의 조각이 일정 확률로 등장합니다.</h1>
+        <h1 className="fs-24 notoBold drawNotice">전체 뽑기는 216종류의 조각이 중 하나의 조각이 일정 확률로 등장합니다.</h1>
         <h3 className="fs-18 notoMid drawNotice drawNotice2">보유중인 업적에 따라 미보유중인 조각이 등장할 확률이 상승합니다.</h3>
       </div>
     </div>
--- frontend/src/components/Draw/SelectDraw.jsx
+++ frontend/src/components/Draw/SelectDraw.jsx
@@ -1,9 +1,332 @@
 import React from "react";
-import './SelectDraw.scss'
+import "./SelectDraw.scss";
+
+import TEST from "@images/test.png";
 
 function SelectDraw() {
+  function select(e) {
+    document.getElementById(e.currentTarget.id).parentElement.className="selectedCard";
+  }
+  function unSelect(e){
+    document.getElementById(e.currentTarget.id).parentElement.className="drawCard2";
+  }
   return (
-    <div className="SelectDraw">개별뽑기 페이지</div>
-  )
+    <div className="SelectDraw">
+      <div className="CardList flex raw">
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            id="1"
+            onFocus={() => 1}
+            onMouseOver={e => select(e)}
+            onBlur={()=>1}
+            onMouseLeave={e =>unSelect(e)}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            id="2"
+            onFocus={() => 1}
+            onMouseOver={e => select(e)}
+            onBlur={()=>1}
+            onMouseLeave={e =>unSelect(e)}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            id="3"
+            onFocus={() => 1}
+            onMouseOver={e => select(e)}
+            onBlur={()=>1}
+            onMouseLeave={e =>unSelect(e)}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+        <div className="drawCard2">
+          <button
+            className="selectbutton"
+            type="button"
+            onClick={e => {
+              select(e);
+            }}
+          >
+            <img className="drawImg" src={TEST} alt="#" />
+            <h1 className="fs-20 notoBold drawTitle">동물 이름</h1>
+          </button>
+        </div>
+      </div>
+      <button
+        className="drawButton fs-28 notoBold"
+        id="collection"
+        type="button"
+      >
+        선택 뽑기 (가격)
+      </button>
+      <div>
+        <h1 className="fs-24 notoBold drawNotice">
+          뽑기를 통해 컬렉션을 수집해보세요.
+        </h1>
+        <h1 className="fs-24 notoBold drawNotice">
+          선택 뽑기는 선택된 동물 9조각중 하나의 조각이 일정 확률로 등장합니다.
+        </h1>
+        <h3 className="fs-18 notoMid drawNotice drawNotice2">
+          보유중인 업적에 따라 미보유중인 조각이 등장할 확률이 상승합니다.
+        </h3>
+      </div>
+    </div>
+  );
 }
 export default SelectDraw;
