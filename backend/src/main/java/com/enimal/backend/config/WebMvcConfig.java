233d6fed3ed9e4c82a4146ab6bee764dcd65743d
lion1735 <lion1735@naver.com>
2022-09-23 17:43:28 +0900

    feat : 프론트 CORS 처리6 #39

--- backend/src/main/java/com/enimal/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/enimal/backend/config/WebMvcConfig.java
@@ -18,18 +18,18 @@ import java.util.List;
 @PropertySource("classpath:application.yml")
 @EnableAspectJAutoProxy
 public class WebMvcConfig implements WebMvcConfigurer {
-//    public List loginEssential = Arrays.asList(""); //적용할 URL 패턴
-//    public List loginInessential = Arrays.asList("/user/login","/user/profile/**","/user/completion/**","/todayAnimal","/noticeList/**","/boardList/**","/user/nickname/**"); //제외할 URL 패턴
-//    @Bean
-//    public Intercepter intercepter(){
-//        return new Intercepter();
-//    }
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(intercepter())
-//                .addPathPatterns(loginEssential)
-//                .excludePathPatterns(loginInessential);
-//    }
+    public List loginEssential = Arrays.asList(""); //적용할 URL 패턴
+    public List loginInessential = Arrays.asList("/user/login","/user/profile/**","/user/completion/**","/todayAnimal","/noticeList/**","/boardList/**","/user/nickname/**"); //제외할 URL 패턴
+    @Bean
+    public Intercepter intercepter(){
+        return new Intercepter();
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(intercepter())
+                .addPathPatterns(loginEssential)
+                .excludePathPatterns(loginInessential);
+    }
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**")
