3ed80b810ec668d71fd3a10193fc47701b73dc53
김유완 <opal18@naver.com>
2022-09-23 13:26:03 +0900

    test : 첫 게시물 등록 업적 #35

--- backend/src/test/java/com/enimal/backend/BoardTests.java
+++ backend/src/test/java/com/enimal/backend/BoardTests.java
@@ -2,6 +2,7 @@ package com.enimal.backend;
 
 import com.enimal.backend.dto.Board.BoardRegistDto;
 import com.enimal.backend.dto.Board.BoardShowDto;
+import com.enimal.backend.entity.Badge;
 import com.enimal.backend.entity.Board;
 import com.enimal.backend.entity.User;
 import com.enimal.backend.repository.*;
@@ -15,6 +16,7 @@ import org.springframework.data.domain.Slice;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
@@ -22,14 +24,16 @@ import java.util.Optional;
 public class BoardTests {
     UserRepository userRepository;
     BoardRepository boardRepository;
+    BadgeRepository badgeRepository;
     @Autowired
-    public BoardTests( UserRepository userRepository, BoardRepository boardRepository){
+    public BoardTests( UserRepository userRepository, BoardRepository boardRepository, BadgeRepository badgeRepository){
         this.userRepository = userRepository;
         this.boardRepository = boardRepository;
+        this.badgeRepository = badgeRepository;
     }
     @Test
     void 자유게시판_등록(){
-        String userId = "test";
+        String userId = "admin";
         String title = "자유게시판 제목";
         String content = "자유게시판 내용";
         Byte[] picture = {1,2,3,4,5,3,2,1};
@@ -40,6 +44,23 @@ public class BoardTests {
         boardRegistDto.setPicture(picture);
         Board board = new Board();
         Optional<User> user = userRepository.findById(userId);
+        // 업적 4번 : 첫 게시물 작성
+        List<Board> boards = boardRepository.findByUser(user);
+        int size = boards.size();
+        if(size == 0) {
+            Badge badge = new Badge();
+            badge.setBadge("업적 냠냠");
+            badge.setCreatedate(LocalDateTime.now());
+            badge.setUser(user.get());
+            badge.setPercentage(2);
+            badgeRepository.save(badge);
+            System.out.println("뱃지내역");
+            System.out.println(badge.getBadge());
+            System.out.println(badge.getUser().getId());
+            System.out.println(badge.getCreatedate());
+            System.out.println(badge.getPercentage());
+        }
+        else System.out.println(size+"입니다");
         board.setUser(user.get());
         board.setTitle(boardRegistDto.getTitle());
         board.setContent(boardRegistDto.getContent());
