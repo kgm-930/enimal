47663699facf049c49c5b8bc0c0e586b6e04cc14
김유완 <opal18@naver.com>
2022-09-23 21:56:44 +0900

    test : 댓글 10개 이상 달린 경우 업적 획득 #35

--- backend/src/test/java/com/enimal/backend/CommentTests.java
+++ backend/src/test/java/com/enimal/backend/CommentTests.java
@@ -1,9 +1,11 @@
 package com.enimal.backend;
 
 import com.enimal.backend.dto.Comment.CommentShowDto;
+import com.enimal.backend.entity.Badge;
 import com.enimal.backend.entity.Board;
 import com.enimal.backend.entity.Comment;
 import com.enimal.backend.entity.User;
+import com.enimal.backend.repository.BadgeRepository;
 import com.enimal.backend.repository.BoardRepository;
 import com.enimal.backend.repository.CommentRepository;
 import com.enimal.backend.repository.UserRepository;
@@ -24,11 +26,13 @@ public class CommentTests {
     UserRepository userRepository;
     BoardRepository boardRepository;
     CommentRepository commentRepository;
+    BadgeRepository badgeRepository;
     @Autowired
-    public CommentTests( UserRepository userRepository, BoardRepository boardRepository,CommentRepository commentRepository){
+    public CommentTests( UserRepository userRepository, BoardRepository boardRepository,CommentRepository commentRepository, BadgeRepository badgeRepository){
         this.userRepository = userRepository;
         this.boardRepository = boardRepository;
         this.commentRepository = commentRepository;
+        this.badgeRepository = badgeRepository;
     }
     @Test
     void 댓글_등록(){
@@ -43,6 +47,43 @@ public class CommentTests {
         comment.setCreatedate(LocalDateTime.now());
         comment.setUser(user.get());
         commentRepository.save(comment);
+        // 업적 14번 : 게시글에 댓글이 10개 이상 달린 경우
+        List<Comment> commentList = commentRepository.findByBoard_Idx(idx);
+        System.out.println(commentList.size());
+        List<Badge> badgeList = badgeRepository.findByUserId(board.get().getUser().getId());
+        System.out.println(badgeList.size());
+        Boolean flag = true;
+        for(int i=0; i< badgeList.size(); i++){ // 이미 그 글쓴이가 댓글왕인 경우는 제외하기
+            if((badgeList.get(i).getBadge()).equals("댓글왕")) {
+                flag = false;
+                System.out.println("이미 뱃지가 있어요");
+                break;
+            }
+        }
+        System.out.println(flag);
+        if(commentList.size() >= 10 && flag){ // 일단 10개 이상인지 확인 후 글 작성자가 작성한 댓글은 제외하기
+            String writerId = board.get().getUser().getId();
+            System.out.println("writerId"+writerId);
+            int count = 0;
+            for (int i=0; i<commentList.size(); i++){
+                if((commentList.get(i).getUser().getId()).equals(writerId)){
+                    System.out.println("스스로 댓글은 안됩니다.");
+                }
+                else {
+                    count++;
+                    System.out.println("댓글왕에 한걸음.");
+                }
+            }
+            if(count >= 10){
+                Badge badge = new Badge();
+                badge.setBadge("댓글왕");
+                badge.setCreatedate(LocalDateTime.now());
+                badge.setUser(board.get().getUser());
+                badge.setPercentage(2);
+                badgeRepository.save(badge);
+                System.out.println("댓글왕!!!");
+            }
+        }
         System.out.println(comment.getContent());
         System.out.println(comment.getUser().getId());
         System.out.println(comment.getBoard().getIdx());
